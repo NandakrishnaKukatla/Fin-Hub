025c253ef38351105275b42599cff8225d956a1c
NandakrishnaKukatla <nandakrishna0794@gmail.com>
2026-04-27 22:56:51 +0530

    feat: implement user authentication with login, sign-up components, and environment configuration

--- Frontend/src/components/Login/index.jsx
+++ Frontend/src/components/Login/index.jsx
@@ -60,7 +60,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch(`${Base_url}/api/auth/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
