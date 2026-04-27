359d29c56b258804ac8e69f01b4c916e0fc489ad
NandakrishnaKukatla <nandakrishna0794@gmail.com>
2026-04-27 11:44:13 +0530

    Login issues resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,16 @@ import Home from "./components/Home/index.jsx";
 import SignUp from "./components/SignUp/index.jsx";
 import Navbar from "./components/Nav-Bar/index.jsx";
 import Expenses from "./components/Expenses/index.jsx";
+import Cookies from "js-cookie";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const ProtectedRoute = () => {
+  const jwtToken = Cookies.get("token");
+  if (!jwtToken) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />;
+};
 
 const Layout = () => (
   <>
@@ -11,18 +21,19 @@ const Layout = () => (
     <Outlet />
   </>
 );
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "your_google_client_id"}>
       <BrowserRouter>
         <Routes>
-          {/* Everything inside this Layout route will share the Navbar */}
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/expenses" element={<Expenses />} />
+          <Route element={<ProtectedRoute />}>
+            {/* Everything inside this Layout route will share the Navbar */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/expenses" element={<Expenses />} />
+            </Route>
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
